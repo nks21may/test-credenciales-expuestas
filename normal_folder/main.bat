a3ab1ba13a8074361e43f62fd109ae922b8fbabb
Nicolás D'Alessandro <nicolasdalessandro2@gmail.com>
2023-07-10 19:36:17 -0300

    .

--- normal_folder/main.bat
+++ normal_folder/main.bat
@@ -5,5 +5,5 @@ AWS_SESSION_TOKEN="session.token"
 DB_USERNAME="credencial1";
 DB_USERNAME='credencial2';
 
-TELEGRAM-API-KEY:"asklmnasdkljasd"
-TELEGRAM-API-KEY=asklmnasdkljasd
+TELEGRAM-API-KEY:"credencial3"
+TELEGRAM-API-KEY=credencial4
